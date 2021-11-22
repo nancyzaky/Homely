0c40819dbc063b48e18a9e2581651edd828ae87d
Nancy Zaky <nancyzki@yahoo.com>
2021-11-21 16:25:41 -0800

    .

--- app/controllers/api/fallback_controller.rb
+++ app/controllers/api/fallback_controller.rb
@@ -1,6 +1,6 @@
 # Controller logic: fallback requests for React Router.
 # Leave this here to help deploy your app later!
-class FallbackController < ActionController::Base
+class Api::FallbackController < ActionController::Base
 
   def index
     # React app index page
