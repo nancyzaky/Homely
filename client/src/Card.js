57b01e6930be7dec438e863ef3132ed33b675e9b
Nancy Zaky <nancyzki@yahoo.com>
2021-11-22 17:56:56 -0800

    modified card component

--- client/src/Card.js
+++ client/src/Card.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion, useMotionValue, useTransform } from "framer-motion";
-import Model from "./Model";
 
-const Card = ({ item }) => {
+const Card = ({ item, text, colorTwo, code, codeApplied, setCodeApplied }) => {
   const x = useMotionValue(0);
   const y = useMotionValue(0);
   const rotateX = useTransform(x, [100, -100], [30, -30]);
   const rotateY = useTransform(y, [100, -100], [-30, 30]);
+  const [revealCode, setRevealCode] = useState(false);
+  const [error, setError] = useState(false);
 
   return (
     <motion.div className="card-anime" whileHover={{ scale: "1.1" }}>
@@ -20,7 +21,7 @@ const Card = ({ item }) => {
       >
         <div className="upper-cont">
           <div className="card-wrapper">
-            <div className="circle"></div>
+            <div className="circle" style={{ backgroundImage: colorTwo }}></div>
           </div>
           <div className="furn-wrapr">
             <div className="furn">
@@ -35,9 +36,30 @@ const Card = ({ item }) => {
               ></motion.img>
             </div>
           </div>
-          <h1 className="card-text">SOFA</h1>
+          <h3 className="card-text">{text}</h3>
         </div>
-        <div className="lower-cont"></div>
+        <div className="lower-cont">
+          <button
+            className="btn"
+            style={{ marginLeft: "6rem", marginTop: "3rem" }}
+            onClick={() => {
+              if (!codeApplied) {
+                setRevealCode(true);
+                setCodeApplied(true);
+              } else {
+                setError(true);
+              }
+            }}
+          >
+            Reveal Code
+          </button>
+        </div>
+        {revealCode && (
+          <h3 style={{ textAlign: "center" }}>Discount Code: {code}</h3>
+        )}
+        {error && (
+          <h3 style={{ textAlign: "center" }}>Sorry, You revealed one card</h3>
+        )}
       </motion.div>
     </motion.div>
   );
