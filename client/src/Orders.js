64d9ef424f19a648f82c28d75d2c831007bce432
Nancy Zaky <nancyzki@yahoo.com>
2021-11-23 14:06:03 -0800

    added orders component

---
+++ client/src/Orders.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+
+const Orders = () => {
+  useEffect(() => {
+    fetch("/api/me")
+      .then((resp) => resp.json())
+      .then((d) => {
+        if (d.id > 0) {
+          fetch(`/api/shops/${d.id}`)
+            .then((resp) => resp.json())
+            .then((d) => console.log(d));
+        }
+      });
+  }, []);
+  return <div style={{ overFlow: "hidden" }}></div>;
+};
+
+export default Orders;
